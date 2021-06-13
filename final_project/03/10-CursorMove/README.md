6314557d866b47a440c2f3fcffb92e792b0b7bbf
FUYUHSUAN <ramy1231863@gmail.com>
2021-06-13 16:08:47 +0800

    add

---
+++ final_project/03/10-CursorMove/README.md
@@ -0,0 +1,43 @@
+# 讓光標能上下左右移動
+
+這段程式執行時，能夠讓光標自由上下左右移動
+
+* `strlen()` :來自<string.h>
+
+* enum :列舉，一邊多用於只有少數值得類型
+  ```c
+  enum editprKey {  //在列舉中第一個設為1000，其他會依序設成1001,1002,1003
+    ARROW_LEFT  = 1000,
+    ARROW_RIGHT ,
+    ARROW_UP ,
+    ARROW_DOWN 
+  };
+  ```
+
+---
+* 運用上下左右鍵讓光標移動
+  ```c
+  void editorMoveCursor(int key){  //運用上下左右鍵讓光標移動
+    switch(key) {
+      case ARROW_LEFT:
+        E.cx--;
+        break;
+      case ARROW_RIGHT:
+        E.cx++;
+        break;
+      case ARROW_UP:
+        E.cy--;
+        break;
+      case ARROW_DOWN:
+        E.cy++;
+        break;
+    }
+  }
+  ```
+---
+**參考資料:**
+
+
+
+* [kilo](https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html)
+
--- final_project/03/11-PageUp&Down/kilo.c
+++ final_project/03/11-PageUp&Down/kilo.c
@@ -19,7 +19,9 @@ enum editprKey {  //在列舉中第一個設為1000，其他會依序設成1001,
   ARROW_LEFT  = 1000,
   ARROW_RIGHT ,
   ARROW_UP ,
-  ARROW_DOWN 
+  ARROW_DOWN ,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -77,11 +79,21 @@ int editorReadKey() {
     if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if(seq[0] == '['){
-      switch(seq[1]){
-        case 'A' : return ARROW_UP;
-        case 'B' : return ARROW_DOWN;
-        case 'C' : return ARROW_RIGHT;
-        case 'D' : return ARROW_LEFT;
+      if(seq[1] >= '0' && seq[1] <= '9'){
+        if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if(seq[2] == '~'){
+          switch(seq[1]){
+            case '5' : return PAGE_UP;
+            case '6' : return PAGE_DOWN;
+          }
+        }
+      }else{
+        switch(seq[1]){
+          case 'A' : return ARROW_UP;
+          case 'B' : return ARROW_DOWN;
+          case 'C' : return ARROW_RIGHT;
+          case 'D' : return ARROW_LEFT;
+        }
       }
     }
 
@@ -197,16 +209,24 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key){  //運用上下左右鍵讓光標移動
   switch(key) {
     case ARROW_LEFT:
-      E.cx--;
+      if(E.cx != 0){
+        E.cx--;
+      }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if(E.cx != E.screencols-1){
+        E.cx++;
+      }
       break;
     case ARROW_UP:
-      E.cy--;
+      if(E.cy != 0){
+        E.cy--;
+      }
       break;
     case ARROW_DOWN:
-      E.cy++;
+      if(E.cy != E.screenrows -1){
+        E.cy++;
+      }
       break;
   }
 }
@@ -220,6 +240,14 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP :ARROW_DOWN);         
+      }
 
     case ARROW_UP:
     case ARROW_DOWN:
