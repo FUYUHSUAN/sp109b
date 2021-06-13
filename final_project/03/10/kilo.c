c00a59ec398aaea779a069ea2f79572ac3af5460
FUYUHSUAN <ramy1231863@gmail.com>
2021-06-13 14:21:21 +0800

    add

--- final_project/03/10/kilo.c
+++ final_project/03/10/kilo.c
@@ -155,10 +155,11 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
   abAppend(&ab, "\x1b[?25h", 6);
 
-  abAppend(&ab, "\x1b[H", 3);
+  //abAppend(&ab, "\x1b[H", 3);
   abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
