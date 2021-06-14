1af986449e279e822f1b6bc82a6819a92324aa56
FUYUHSUAN <ramy1231863@gmail.com>
2021-06-14 20:34:22 +0800

    add

--- final_project/04/05/kilo.c
+++ final_project/04/05/kilo.c
@@ -300,11 +300,19 @@ void editorMoveCursor(int key){  //運用上下左右鍵讓光標移動
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch(key) {
     case ARROW_LEFT:
-        E.cx--;
+        if(E.cx != 0){
+          E.cx--;
+        } else if(E.cy > 0){
+          E.cy--;
+          E.cx = E.row[E.cy].size;
+        }
       break;
     case ARROW_RIGHT:
       if(row && E.cx < row->size){
         E.cx++;
+      }else if(row && E.cx == row->size){
+        E.cy++;
+        E.cx = 0;
       }
       break;
     case ARROW_UP:
@@ -317,6 +325,12 @@ void editorMoveCursor(int key){  //運用上下左右鍵讓光標移動
         E.cy++;
       }
       break;
+  }
+
+  row = (E.cy >= E.numrows)?NULL :&E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if(E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
