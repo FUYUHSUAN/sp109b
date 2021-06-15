a940bd26fd56fce41358ac3043fe08c78217d6f4
FUYUHSUAN <ramy1231863@gmail.com>
2021-06-16 00:12:02 +0800

    add

--- final_project/05/03/kilo.c
+++ final_project/05/03/kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -509,6 +510,12 @@ void editorProcessKeypress() {
       break;
       
     case CTRL_KEY('w'):
+      if (E.dirty && quit_times > 0) {
+        editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+          "Press Ctrl-Q %d more times to quit.", quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -565,6 +572,7 @@ void editorProcessKeypress() {
       editorInsertChar(c);
       break;
   }
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
